53be571cdf97c71ddebb4ea0a8371efe550e7424
Vaishnavi359 <147237685+Atlastjio359@users.noreply.github.com>
2023-10-10 19:59:35 +0530

    Create 02. C Program to Print an Integer (Entered by the User).c

---
+++ 02. C Program to Print an Integer (Entered by the User).c
@@ -0,0 +1,12 @@
+// 25-days-75-Questions-C-Coding-challange
+//Question number 2 : C Program to Print an Integer (Entered by the User)
+
+#include<stdio.h>
+int main()
+{
+    int n; //assume n is integer
+    printf("Enter an integer\n");
+    scanf("%d",&n );
+    printf("Your entered integer is:- %d",n);
+    return 0;
+}
